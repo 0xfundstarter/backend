737be6908efb03196882872824c4c5f72c7c3d04
0xshikhar <shikhar@iiitmanipur.ac.in>
2023-04-22 02:36:27 +0530

    updated

--- contracts/ProjectNFT.sol
+++ contracts/ProjectNFT.sol
@@ -7,11 +7,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ProjectNFT is ERC721URIStorage, Ownable {
+contract ProjectNFT is ERC721URIStorage,ERC721Enumerable Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("KickNFT", "Project") {}
+    constructor() ERC721("FundStarter", "NFT Collection") {}
 
     function mintNFT1(string memory tokenURI1) public payable {
         _tokenIds.increment();
@@ -20,6 +20,41 @@ contract ProjectNFT is ERC721URIStorage, Ownable {
 
         _safeMint(msg.sender, newTokenId);
         _setTokenURI(newTokenId, tokenURI1);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 
 }
